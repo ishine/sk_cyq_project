7c9fe3fb216d720d9fdb87e57198ef7d07e1ebfe
kunsung <1462110775@qq.com>
2020-12-10 21:34:16 +0800

    modify the train code

--- train.py
+++ train.py
@@ -7,8 +7,8 @@ import torch.optim as optim
 import time
 
 from torch.utils.data import DataLoadaer
-from hparams import hparams as hp
-from dataset import lpcnetDataset
+
+from utils.dataset import lpcnetDataset
 from models.model import Model
 
 def create_model(hp):
@@ -22,9 +22,53 @@ def create_model(hp):
         training=True)
     return model
 
+def save_checkpoint(args, model, optimizer, step):
+    checkpoint_path = os.path.join(args.checkpoint_dir, "model.ckpt-{}.pt".format(step))
+
+    torch.save({"model": model.state_dict(),
+                "optimizer": optimizer.state_dict(),
+                "global_step": step
+                }, checkpoint_path)
+
+    print("Saved checkpoint: {}".format(checkpoint_path))
+
+    with open(os.path.join(args.checkpoint_dir, 'checkpoint'), 'w') as f:
+        f.write("model.ckpt-{}.pt".format(step))
+
+def attempt_to_restore(model, optimizer, checkpoint_dir, use_cuda):
+    checkpoint_list = os.path.join(checkpoint_dir, 'checkpoint')
+
+    if os.path.exists(checkpoint_list):
+        checkpoint_filename = open(checkpoint_list).readline().strip()
+        checkpoint_path = os.path.join(
+            checkpoint_dir, "{}".format(checkpoint_filename))
+        print("Restore from {}".format(checkpoint_path))
+        checkpoint = load_checkpoint(checkpoint_path, use_cuda)
+        model.load_state_dict(checkpoint["model"])
+        optimizer.load_state_dict(checkpoint["optimizer"])
+        global_step = checkpoint["global_step"]
+
+    else:
+        global_step = 0
+
+    return global_step
+
+def load_checkpoint(checkpoint_path, use_cuda):
+    if use_cuda:
+        checkpoint = torch.load(checkpoint_path)
+    else:
+        checkpoint = torch.load(
+            checkpoint_path, map_location=lambda storage, loc: storage)
+
+    return checkpoint
+
 def train(args):
     
     os.makedirs(args.checkpoint_dir, exist_ok=True)
+
+    hparams = YParams(args.yaml_conf)
+    hp = hparams.parse(args.hparams) 
+
     train_dataset = lpcnetDataset(args)
     device = torch.device('cuda:0' if args.use_cuda else 'cpu')
     model = create_model(hp)
@@ -53,10 +97,10 @@ def train(args):
     customer_optimizer = Optimizer(optimizer, args.learning_rate, global_step, args.warmup_steps, args.decay_learning_rate)
     
     log_scales = -7.0
-    t_start = 2000
-    t_end   = 40000
-    interval = 400
-    density = (0.05, 0.05, 0.2)
+    t_start = hp.t_start
+    t_end   = hp.t_end
+    interval = hp.interval
+    density = hp.density
 
     for epoch in range(args.epochs):
         train_data_loader = DataLoader(train_dataset, batch_size = args.batch_size, num_workers=args.num_workers, shuffle=True, pin_memory=True)
@@ -68,7 +112,7 @@ def train(args):
             else:
                 output_predict = model(in_data, mel) 
             
-            loss = discretized_mix_logistic_loss(output, output_predict, log_scales)   
+            loss = discretized_mix_logistic_loss(output, output_predict, log_scales).to(device)   
          
             global_step += 1
             customer_optimizer.zero_grad()
@@ -86,4 +130,27 @@ def train(args):
           
             if global_step % args.summary_step == 0:
                 writer.add_scalar("loss", loss.item(), global_step)
-             
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file_list', type=str, default='', help='Directory of file list')
+    parser.add_argument('--mel_dir', type=str, default='', help='Directory of mel')
+    parser.add_argument('--pcm_dir', type=str, default='', help='Directory of pcm')
+    parser.add_argument('--num_workers',type=int, default=4, help='Number of dataloader workers.')
+    parser.add_argument('--epochs', type=int, default=50000)
+    parser.add_argument('--checkpoint_dir', type=str, default="logdir", help="Directory to save model")
+    parser.add_argument('--resume', type=str, default=None, help="The model name to restore")
+    parser.add_argument('--checkpoint_step', type=int, default=5000)
+    parser.add_argument('--summary_step', type=int, default=100)
+    parser.add_argument('--use_cuda', type=_str_to_bool, default=True)
+    parser.add_argument('--keep_training', type=_str_to_bool, default=True)
+    parser.add_argument('--learning_rate', type=float, default=0.001)
+    parser.add_argument('--warmup_steps', type=int, default=80000)
+    parser.add_argument('--decay_learning_rate', type=float, default=0.5)
+    parser.add_argument('--batch_size', type=int, default=60)
+    parser.add_argument('--yaml_conf', default='hparams.yaml', help='yaml files for configurations.')
+    parser.add_argument('--hparams', default='')
+
+    args = parser.parse_args()
+    train(args)
